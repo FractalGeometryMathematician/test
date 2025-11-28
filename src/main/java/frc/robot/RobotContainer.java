7687f752e25ce80dee101b9639625f3b19f82b59
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2025-11-28 11:12:36 -0800

    implement drive subsystem in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,10 +8,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.OuttakeSubsystem;
 
 public class RobotContainer {
   private final OuttakeSubsystem m_outtakeSubsystem = new OuttakeSubsystem();
+  private final DriveSubsystem m_driveSubsystem = new DriveSubsystem();
 
   private final CommandPS5Controller m_driverController = new CommandPS5Controller(
       OperatorConstants.kDriverControllerPort);
@@ -20,7 +22,14 @@ public class RobotContainer {
     configureBindings();
 
     m_outtakeSubsystem.setDefaultCommand(
-        m_outtakeSubsystem.VelocityControl(() -> m_driverController.getL2Axis(), () -> m_driverController.getR2Axis()));
+        m_outtakeSubsystem.VelocityControl(
+            () -> m_driverController.getL2Axis(),
+            () -> m_driverController.getR2Axis()));
+
+    m_driveSubsystem.setDefaultCommand(
+        m_driveSubsystem.tankDrive(
+            () -> m_driverController.getLeftY(),
+            () -> m_driverController.getRightY()));
   }
 
   private void configureBindings() {
